3797be5ee620afe16955c3e694b6dd3b4231ad59
KoKoLates <the21515@gmail.com>
2022-08-20 02:28:41 +0800

    add the parts of python version check

--- misc/setup.sh
+++ misc/setup.sh
@@ -1,2 +1,19 @@
 #! /usr/bin/bash
 
+sudo apt update
+sudo apt-get update
+python_version=`python --version 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
+
+if (! $python_version); then
+    echo "[ERROR] No python installed."
+    sudo apt install python3 -y
+fi
+
+if ($python_version == 3); then
+    echo "The python version is 3"
+elif ($python_version == 2); then
+    echo "The python version is 2"
+fi
+
+pip install python-opencv opencv-contrib -y
+pip install numpy
